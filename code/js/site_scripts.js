a4061eaf3434c0ed89fe06afcb53007307904849
Seale Rapolai <1098005@students.wits.ac.za>
2017-09-29 16:22:10 +0200

    Updated login pages and created custermer pages (navbar).

--- code/js/site_scripts.js
+++ code/js/site_scripts.js
@@ -1,4 +1,12 @@
+var server_URL = "";
 $(document).ready(function() {
+	$('#btn-create-account').click(function(){
+		window.open("/register_cust", "_self");
+	});
+	$('#btn-cancel-regist').click(function(){
+		window.open("/customer", "_self");
+	});
+
 	$('#btn-submit-regis').submit(function (){
 		
 	});
--- code/server.js
+++ code/server.js
@@ -14,19 +14,23 @@ app.use(express.static('public'));
 /***********************************************************/
 //Serve Web Pagess
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/login_cust.html');
+    res.sendFile(__dirname + '/html/login_cust.html');
 });
 
 app.get('/customer', function(req, res){
-    res.sendFile(__dirname + '/login_cust.html');
+    res.sendFile(__dirname + '/html/login_cust.html');
+});
+
+app.get('/register_cust', function(req, res){
+    res.sendFile(__dirname + '/html/Register_cust.html');
 });
 
 app.get('/broker', function(req, res){
-    res.sendFile(__dirname + '/login_broker.html');
+    res.sendFile(__dirname + '/html/login_broker.html');
 });
 
 app.get('/manager', function(req, res){
-    res.sendFile(__dirname + '/login_manager.html');
+    res.sendFile(__dirname + '/html/login_manager.html');
 });
 
 //Serves all and any of the static files
