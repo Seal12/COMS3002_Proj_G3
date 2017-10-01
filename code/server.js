7d9f2faff828d9fc80ad8a5b615526ff87c1a31f
Seale Rapolai <1098005@students.wits.ac.za>
2017-10-01 20:51:32 +0200

    Updated server.js and created manager sales page.

--- code/server.js
+++ code/server.js
@@ -11,28 +11,58 @@ app.use(express.static('public'));
 /***********************************************************/
 /************************Serve files************************/
 /***********************************************************/
-//Serve Web Pagess
+//Customer
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/html/login_cust.html');
 });
-
 app.get('/customer', function(req, res){
     res.sendFile(__dirname + '/html/login_cust.html');
 });
-
 app.get('/register_cust', function(req, res){
     res.sendFile(__dirname + '/html/Register_cust.html');
 });
+app.get('/customer/home', function(req, res){
+    res.sendFile(__dirname + '/html/home_customer.html');
+});
+app.get('/customer/preferences', function(req, res){
+    res.sendFile(__dirname + '/html/preferences_customer.html');
+});
+app.get('/customer/sell', function(req, res){
+    res.sendFile(__dirname + '/html/sell_home.html');
+});
+app.get('/customer/account', function(req, res){
+    res.sendFile(__dirname + '/html/account_customer.html');
+});
 
+//Broker
 app.get('/broker', function(req, res){
     res.sendFile(__dirname + '/html/login_broker.html');
 });
+app.get('/broker/home', function(req, res){
+    res.sendFile(__dirname + '/html/home_broker.html');
+});
+app.get('/broker/timeTable', function(req, res){
+    res.sendFile(__dirname + '/html/time_table.html');
+});
+app.get('/broker/account', function(req, res){
+    res.sendFile(__dirname + '/html/account_broker.html');
+});
 
+//Manager
 app.get('/manager', function(req, res){
     res.sendFile(__dirname + '/html/login_manager.html');
 });
+app.get('/manager/home', function(req, res){
+    res.sendFile(__dirname + '/html/home_manager.html');
+});
+app.get('/manager/sales', function(req, res){
+    res.sendFile(__dirname + '/html/sales_manager.html');
+});
+app.get('/broker/account', function(req, res){
+    res.sendFile(__dirname + '/html/account_manager.html');
+});
 
-//Serves all and any of the static files
+//Serves all and any of the static files. SUch as CSS, JS and images.
 app.get(/^(.+)$/, function(req, res){ 
 	res.sendFile( __dirname + req.params[0]); 
 });
